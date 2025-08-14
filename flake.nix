fca4d3ff6e63f2d34d5d2e9de70e03ceb76d4792
Evan Travers <evantravers@gmail.com>
2025-08-14 07:43:26 -0500

    Setup basic flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          lexical
+          elixir_1_19
+        ];
+      };
+    }
+  );
+}
